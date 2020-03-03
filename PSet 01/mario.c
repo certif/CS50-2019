06cce89a4a99310049ff577bf6c108431f665e7e
Oleksandr Tkachov <certifis@gmail.com>
2020-03-03 22:14:32 +0200

    feat: add solution task Mario

---
+++ PSet 01/mario.c
@@ -0,0 +1,27 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    
+     do {
+
+	   n = get_int("Height: ");
+
+        } 
+    while (n < 1 || n > 8);
+    
+    for (int i = 1; i < n + 1; i++)
+      {
+              for (int j = n; j > i; j--)
+          {         
+                printf(" ");
+          }
+               for (int z = 0; z < i ; z++)
+          {         
+                printf("#");
+          } 
+      printf("\n"); 
+        }
+}
